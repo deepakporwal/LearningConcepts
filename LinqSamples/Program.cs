4ea408c1461ae901087052c77dce9569031c157e
DESKTOP-H6FUKM1\deepak <er.deepakporwal@gmail.com>
2021-07-30 09:34:17 +0100

    Add project files.

---
+++ LinqSamples/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jai Shree Ram ! , Welcome to LINQ");
+
+            // declare an string array
+            string[] words = {"deepak","appu","pratik","vipin","avika" };
+            
+            // select words length > 5
+            var shortWords = from word in words where word.Length >= 5 select word;
+
+            foreach (var word in shortWords)
+            {
+                Console.WriteLine(word);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
